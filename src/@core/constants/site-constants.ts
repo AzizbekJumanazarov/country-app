598b58883b638c35393cfee8b02eb8e00c630cfa
Azizbek Jumanazarov <azizbek.jumanazarov@ssdsoftwaresolutions.com>
2024-10-16 00:52:38 +0500

    Add custom constants, updating gitignore

---
+++ src/@core/constants/site-constants.ts
@@ -0,0 +1,13 @@
+type SiteConstants = {
+  MIN_YEAR: number;
+  MAX_YEAR: number;
+  NUMBER_OF_RANDOM_COUNTRY: number;
+};
+
+const siteConstants: SiteConstants = {
+  MIN_YEAR: 2020,
+  MAX_YEAR: 2030,
+  NUMBER_OF_RANDOM_COUNTRY: 3,
+};
+
+export default siteConstants;
